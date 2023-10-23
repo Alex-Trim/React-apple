67516a544fcaab0d49b01e9d5b5f46c6dd2b5ccd
AlexTrim <trimaylovalexey@gmail.com>
2023-10-23 14:12:03 +0500

    start project 0.2

---
+++ my-app/src/components/Categories.jsx
@@ -0,0 +1,15 @@
+function Categories() {
+    return(
+      <div class="categories">
+        <ul class="list--reset categories__list ">
+          <li class="categories__item categories__item--active">Все</li>
+          <li class="categories__item">Мясные</li>
+          <li class="categories__item">Вегетарианская</li>
+          <li class="categories__item">Гриль</li>
+          <li class="categories__item">Острые</li>
+          <li class="categories__item">Закрытые</li>
+        </ul>
+      </div>
+    )
+  }
+  export default Categories;
