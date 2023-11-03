1233544e023675a49b9e2894a205a908542b17ce
AlexTrim <trimaylovalexey@gmail.com>
2023-11-03 11:45:18 +0500

    start project 0.8

--- my-app/src/pagers/Cart.jsx
+++ my-app/src/pagers/Cart.jsx
@@ -24,6 +24,63 @@ export const Cart = () => {
                 </button>
             </div>
         </div>
+        <div className='cart_content'>
+            <ul className='list--reset  content__items'>
+                <li className='content__item'>
+                    <div className='cart-product'>
+                        <div className='cart-product__img'>
+                            <img src="./assets/img/apple-green.png" alt="" />
+                        </div>
+                        <div className='cart-product__description'>
+                            <h3 className='cart-product__heading'>Сырный цыпленок</h3>
+                            <p className='cart-product__text'>тонкое тесто, 26 см.</p>
+                        </div>
+                        <div className='cart-product__count'>
+                            <button className='button cart-product__button'>
+                                <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z" fill="#e02444"></path>
+                                </svg>
+                            </button>
+                            <p className='cart-product__heading'>2</p>
+                            <button className='button cart-product__button'>
+                                <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" fill="#e02444"></path>
+                                    <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z" fill="#e02444"></path>
+                                </svg>
+                            </button>
+                        </div>
+                        <div>
+                            <p className='cart-product__heading'>770 ₽</p>
+                        </div>
+                        <div>
+                            <button className='button cart-product__button cart-product__button--del'>
+                                <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" fill="#b9b9b9"></path>
+                                    <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z" fill="#b9b9b9"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="cart__bottom">
+              <div class="cart__bottom-details">
+                <span> Всего пицц: <b>3 шт.</b> </span>
+                <span> Сумма заказа: <b>900 ₽</b> </span>
+              </div>
+              <div class="cart__bottom-buttons">
+                <a href="/" class="button button--outline button--add go-back-btn">
+                    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                  <span>Вернуться назад</span>
+                </a>
+                <div class="button pay-btn">
+                  <span>Оплатить сейчас</span>
+                </div>
+              </div>
+            </div>
     </div>
   )
 }
